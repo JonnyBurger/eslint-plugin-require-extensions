1bf2e2fd01b1b9c25738a30572197f780c6a54d3
Jonny Burger <jonathanburger11@gmail.com>
2023-02-26 13:36:01 +0100

    update

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
-const { existsSync } = require('fs');
-const { dirname, resolve } = require('path');
+const { existsSync } = require("fs");
+const { dirname, resolve } = require("path");
 
 module.exports = {
-    configs: {
-        recommended: {
-            plugins: ['require-extensions'],
-            rules: {
-                'require-extensions/require-extensions': 'error',
-            },
-        },
+  configs: {
+    recommended: {
+      plugins: ["validate-esm"],
+      rules: {
+        "validate-esm/require-extensions": "error",
+      },
     },
-    rules: {
-        'require-extensions': {
-            meta: {
-                fixable: true,
-            },
-            create(context) {
-                function rule(node) {
-                    const source = node.source;
-                    if (!source) return;
-                    const value = source.value;
-                    if (!value || !value.startsWith('.') || value.endsWith('.js')) return;
+  },
+  rules: {
+    "require-extensions": {
+      meta: {
+        fixable: true,
+      },
+      create(context) {
+        function rule(node) {
+          const source = node.source;
+          if (!source) return;
+          const value = source.value;
+          if (!value || !value.startsWith(".") || value.endsWith(".js")) return;
 
-                    if (!existsSync(resolve(dirname(context.getFilename()), value))) {
-                        context.report({
-                            node,
-                            message: 'Relative imports and exports must end with .js',
-                            fix(fixer) {
-                                return fixer.replaceText(source, `'${value}.js'`);
-                            },
-                        });
-                    }
-                }
+          const filePath = resolve(dirname(context.getFilename()), value);
+          if (!existsSync(filePath)) {
+            context.report({
+              node,
+              message: "Relative imports and exports must end with .js",
+              fix(fixer) {
+                return fixer.replaceText(source, `'${value}.js'`);
+              },
+            });
+            return;
+          }
+          if (lstatSync(filePath).isDirectory()) {
+            context.report({
+              node,
+              message: "Don't import a directory, add /index.js",
+              fix(fixer) {
+                return fixer.replaceText(source, `'${value}/index.js'`);
+              },
+            });
+          }
+        }
 
-                return {
-                    DeclareExportDeclaration: rule,
-                    DeclareExportAllDeclaration: rule,
-                    ExportAllDeclaration: rule,
-                    ExportNamedDeclaration: rule,
-                    ImportDeclaration: rule,
-                };
-            },
-        },
-    }
+        return {
+          DeclareExportDeclaration: rule,
+          DeclareExportAllDeclaration: rule,
+          ExportAllDeclaration: rule,
+          ExportNamedDeclaration: rule,
+          ImportDeclaration: rule,
+        };
+      },
+    },
+  },
 };
